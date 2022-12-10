8ba857892649d3e9c5fb93fb10538b54367040e4
Christos Nikou <chrisnick92@gmail.com>
2022-12-10 14:16:45 +0200

    Solutions for Day9

--- Day9/sol_day9.py
+++ Day9/sol_day9.py
@@ -33,12 +33,19 @@ class rope():
     def move_knot(self, curr_knot: point,
                     front_knot: point) -> point:
         x,y = curr_knot
-        directions = [(1,0), (-1,0), (0,1), (0,-1), (1,1), (-1,1),
-                    (-1,-1), (1,-1)] # [East (E), West (W), North (N), South (S), NE, NW, SW, SE]
-        for i,j in directions:
-            x_prime, y_prime = x+i, y+j
-            if self.condition((x_prime,y_prime), front_knot):
-                return x_prime, y_prime
+        verticals = [(1,0), (-1,0), (0,1), (0,-1)] # [East (E), West (W), North (N), South (S)]
+        diagonals = [(1,1), (-1,1), (-1,-1), (1,-1)] # [NE, NW, SW, SE]
+        
+        if manhattan_dist(curr_knot, front_knot) < 3: # Make a vertical move
+            for i, j in verticals:
+                x_prime,y_prime = x+i, y+j
+                if self.condition((x_prime, y_prime), front_knot):
+                    return x_prime, y_prime
+        elif manhattan_dist(curr_knot, front_knot) >= 3: # Make a diagonal move
+                for i, j in diagonals:
+                    x_prime,y_prime = x+i, y+j
+                    if self.condition((x_prime, y_prime), front_knot):
+                        return x_prime, y_prime
 
         
 def move_head(curr_head_pos: point, move: str) -> point:
@@ -61,11 +68,11 @@ def moves(file: str = 'input.txt'):
 def manhattan_dist(x,y):
     return abs(x[0]-y[0]) + abs(x[1]-y[1])
         
-def track_tail(length: int = 2) -> Set[point]: # Length of rope
+def track_tail(file: str = 'input.txt',
+               length: int = 2) -> Set[point]: # Length of rope
     r = rope(length=length)
-    visited = set()
-    
-    for move, steps in moves():
+    visited = {(0,0)}
+    for move, steps in moves(file):
         for step in range(steps):
             r.update(move_head(r.knots[0], move))
             if r.tail_moved:
@@ -79,5 +86,5 @@ def track_tail(length: int = 2) -> Set[point]: # Length of rope
 
 if __name__ == '__main__':
     print(f"- Part 1: {len(track_tail(length=2))}")
-
+    print(f"- Part 2: {len(track_tail(length=10))}")
 # %%
